1d8b1f5d9e7854e41032fd992a0c3811d9ba57d7
Luis Marquez <luisjose0527@gmail.com>
2023-05-04 13:30:15 -0400

    personalizando las tarjetas

--- lib/src/pages/cards_page.dart
+++ lib/src/pages/cards_page.dart
@@ -13,43 +13,56 @@ class CardPage extends StatelessWidget {
         padding: EdgeInsets.all(10.0),
         children: [
           _cardTipo1(),
-          SizedBox(height: 30.0,),
+          SizedBox(
+            height: 30.0,
+          ),
           _cardTipo2(),
-
         ],
       ),
     );
   }
-  
+
   Widget _cardTipo1() {
     return Card(
+      elevation: 10.0,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
       child: Column(
         children: [
           ListTile(
-            leading: Icon(Icons.photo_album, color: Colors.indigo,),
+            leading: Icon(
+              Icons.photo_album,
+              color: Colors.indigo,
+            ),
             title: Text("Titulo de la tarjeta"),
-            subtitle: Text("Este es el subtitulo de la tarjeta. Aqui deberia ir una descripcion de lo que sea que queramos poner en la tarjeta, puede ser cualquier cosa"),
+            subtitle: Text(
+                "Este es el subtitulo de la tarjeta. Aqui deberia ir una descripcion de lo que sea que queramos poner en la tarjeta, puede ser cualquier cosa"),
           ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: [
-              ElevatedButton(onPressed:() {} , child: Text("Cancelar")),
-              SizedBox(width: 10,),
-              ElevatedButton(onPressed:() {} , child: Text("Ok")),
-            ],
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                ElevatedButton(onPressed: () {}, child: Text("Cancelar")),
+                SizedBox(
+                  width: 10,
+                ),
+                ElevatedButton(onPressed: () {}, child: Text("Ok")),
+              ],
+            ),
           ),
         ],
       ),
     );
   }
-  
+
   Widget _cardTipo2() {
-    return Card(
+    final card = Container(
       child: Column(
         children: [
           FadeInImage(
             placeholder: AssetImage('assets/jar-loading.gif'),
-            image: NetworkImage('https://iso.500px.com/wp-content/uploads/2014/07/big-one.jpg'),
+            image: NetworkImage(
+                'https://iso.500px.com/wp-content/uploads/2014/07/big-one.jpg'),
             fadeInDuration: Duration(milliseconds: 200),
             height: 300,
             fit: BoxFit.cover,
@@ -59,6 +72,25 @@ class CardPage extends StatelessWidget {
             child: Text("data"),
           ),
         ],
+      ),
+    );
+
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(30.0),
+        color: Colors.white,
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black26,
+            blurRadius: 10.0,
+            spreadRadius: 2.0,
+            offset: Offset(2.0, 10.0),
+          ),
+        ],
+      ),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(30.0),
+        child: card,
       ),
     );
   }
