21c20173ff7b08ff29666d8cd0b4bbf27a65d1b2
Luis Marquez <luisjose0527@gmail.com>
2023-05-04 14:44:55 -0400

    circular avatar

--- lib/src/pages/avatar_page.dart
+++ lib/src/pages/avatar_page.dart
@@ -8,6 +8,30 @@ class AvatarPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text("Avatar Page"),
+        actions: [
+          Container(
+            padding: EdgeInsets.all(5.0),
+            child: const CircleAvatar(
+              backgroundImage: NetworkImage("https://cdn.dribbble.com/users/323571/screenshots/5412611/batman_4x.jpg"),
+              radius: 25.0,
+            ),
+          ),
+          
+          Container(
+            margin: EdgeInsets.only(right: 10.0),
+            child: CircleAvatar(
+              child: Text('LM'),
+              backgroundColor: Colors.green[200],
+            ),
+          ),
+        ],
+      ),
+      body: Center(
+        child: FadeInImage(
+          image: NetworkImage("https://preview.redd.it/v0gmtto9p4591.jpg?auto=webp&s=d7c435c3240c4eb3605f44f6e1f9e039695e35f6"),
+          placeholder: AssetImage('assets/jar-loading.gif'),
+          fadeInDuration: Duration(milliseconds: 200),
+        ),
       ),
     );
   }
