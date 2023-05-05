778859cb74764f6b7576086dfcc17ccab7a1f1ac
Luis Marquez <luisjose0527@gmail.com>
2023-05-05 10:23:42 -0400

    check box y switch para el slider

--- lib/src/pages/slider_page.dart
+++ lib/src/pages/slider_page.dart
@@ -8,19 +8,22 @@ class SliderPage extends StatefulWidget {
 }
 
 class _SliderPageState extends State<SliderPage> {
-  double _sliderValue = 50.0;
-  
+  double _sliderValue = 150.0;
+  bool _blockCheck = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Slider'),
+        title: const Text('Slider'),
       ),
       body: Container(
         padding: const EdgeInsets.only(top: 30.0),
         child: Column(
           children: [
             _crearSlider(),
+            _crearCheckBox(),
+            _crearSwitch(),
             Expanded(
               child: _crearImagen(),
             ),
@@ -29,28 +32,53 @@ class _SliderPageState extends State<SliderPage> {
       ),
     );
   }
-  
+
   Widget _crearSlider() {
     return Slider(
       min: 50.0,
       max: 350.0,
-      onChanged: (valor) {
+      onChanged: (_blockCheck) ? null: (valor) {
         setState(() {
-           _sliderValue = valor;
+          _sliderValue = valor;
         });
       },
       value: _sliderValue,
       activeColor: Colors.indigoAccent,
       label: "Tamaño de la imagen",
     );
-
   }
-  
+
   Widget _crearImagen() {
-    return Image(
-      image: NetworkImage("https://assets.stickpng.com/images/5ba0d5b144d37f02b751791e.png"),
+    return FadeInImage(
+      placeholder: const AssetImage('assets/jar-loading.gif'),
+      image: const NetworkImage('https://assets.stickpng.com/images/5ba0d5b144d37f02b751791e.png'),
+      fadeInDuration: const Duration(milliseconds: 500),
       width: _sliderValue,
       fit: BoxFit.contain,
-      ); 
+    );
+  }
+
+  Widget _crearCheckBox() {
+    return CheckboxListTile(
+      title: const Text("Bloquear slider con CheckBox"),
+      value: _blockCheck,
+      onChanged: (value) {
+        setState(() {
+          _blockCheck = value!;
+        });
+      },
+    );
+  }
+
+  _crearSwitch() {
+    return SwitchListTile(
+      title: const Text("Bloquear slider con switch"),
+      value: _blockCheck,
+      onChanged: (value) {
+        setState(() {
+          _blockCheck = value;
+        });
+      },
+    );
   }
 }
